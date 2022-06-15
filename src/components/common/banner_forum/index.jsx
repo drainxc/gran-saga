64387e6f91dc41a924b84121c96d23fc0e325e8a
eastcopper <ldh7228@gmail.com>
2022-06-15 15:56:33 +0900

    📄 :: (#13)[Banner] Add Banner_Forum

---
+++ src/components/common/banner_forum/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import * as S from "./styles";
+
+export default function Banner_Forum() {
+  return (
+    <S.Banner>
+      <img
+        src="https://gransaga.com/assets/images/desktop/common/util/banner_forum.png"
+        alt=""
+      />
+    </S.Banner>
+  );
+}
