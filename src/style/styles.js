a6c88879f796f230806e2565d731fa2f1d98df99
eastcopper <ldh7228@gmail.com>
2022-06-20 20:20:10 +0900

    ⚡ :: (#29)[Style] Update scroll

--- src/style/styles.js
+++ src/style/styles.js
@@ -6,24 +6,8 @@ const GlobalStyle = createGlobalStyle`
     overflow-x: hidden;
   }
 
-  main {
-    width: 1920px;
-    height: 937px;
-    margin: 0;
-    overflow: auto;
-    scroll-snap-type: y mandatory;
-    overflow-x: hidden;
-    -ms-overflow-style: none;
-  }
-
-  main::-webkit-scrollbar {
-    display: none;
-  }
-
-  main > div {
-    scroll-snap-align: start; /* 자식 적용 */
-    width: 1920px;
-    height: 937px;
+  html {
+    scroll-behavior: smooth;
   }
 `;
 
