bf0034e2f47927c6d641b2cee61821a4a1e9639e
eastcopper <ldh7228@gmail.com>
2022-06-23 20:14:01 +0900

    ⚡ :: (#31)[App] Update Scroll snap

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import MainPage from "./components/page/mainPage";
+import GlobalStyle from "./style/styles";
+import Banner_Forum from "./components/common/banner_forum";
+import PCVersion from "./components/page/pcVersion";
+import KeyArt from "./components/page/keyart";
+import { useSelector, useDispatch } from "react-redux";
+import { increment } from "./pagemove";
+import Story from "./components/page/story";
+import { useState } from "react";
+import { useEffect } from "react";
 
 function App() {
+  const dispatch = useDispatch();
+
+  const dummy = ["#main", "#pc", "#keyart", "#story"];
+
+  let page =
+    dummy.indexOf(window.location.hash, 0) >= 0
+      ? dummy.indexOf(window.location.hash, 0)
+      : 0;
+  let scroll = false;
+
+  window.location.href = dummy[page];
+
+  window.addEventListener("wheel", (e) => {
+    if (e.deltaY > 50 && !scroll && page < dummy.length - 1) {
+      movePage(1);
+    } else if (e.deltaY < -50 && !scroll && page > 0) {
+      movePage(-1);
+    }
+    if (page !== dummy.indexOf(window.location.hash, 0)) {
+      page = dummy.indexOf(window.location.hash, 0);
+    }
+  });
+
+  const movePage = (n) => {
+    page += n;
+    scroll = true;
+    dispatch(increment(page));
+    window.location.href = dummy[page];
+    setTimeout(() => {
+      scroll = false;
+    }, 1000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Banner_Forum />
+      <MainPage />
+      <PCVersion />
+      <KeyArt />
+      <Story />
+    </>
   );
 }
 
