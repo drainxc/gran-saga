ac37359cd4c0929afa1a82ca2d273168e45884cd
eastcopper <ldh7228@gmail.com>
2022-06-27 17:15:41 +0900

    ⚡ :: (#32)[KeyArt] Update KeyArt

--- src/components/page/keyart/index.jsx
+++ src/components/page/keyart/index.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useRef } from "react";
 import * as S from "./styles";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { image } from "../../../lib/export/data";
+import { useSelector } from "react-redux";
 
 export default function KeyArt() {
   const settings = {
@@ -13,17 +14,41 @@ export default function KeyArt() {
     arrow: true,
   };
 
+  const TextRef1 = useRef(null);
+  const TextRef2 = useRef(null);
+  const KeyArtPicturesRef = useRef(null);
+
+  const page = useSelector((state) => state.movePage.page);
+
+  if (
+    TextRef1.current !== null &&
+    TextRef1.current !== null &&
+    KeyArtPicturesRef.current !== null
+  ) {
+    if (page === 2) {
+      TextRef1.current.classList.add("active");
+      TextRef2.current.classList.add("active");
+      KeyArtPicturesRef.current.classList.add("active");
+    } else {
+      TextRef1.current.classList.remove("active");
+      TextRef2.current.classList.remove("active");
+      KeyArtPicturesRef.current.classList.remove("active");
+    }
+  }
+
   return (
-    <S.Container>
+    <S.Container id="keyart">
       <S.KeyArtDiv>
-        <S.Text top={50} left={5}>
+        <S.Text top={20} left={5}>
           <img
+            ref={TextRef1}
             src="https://gransaga.com/assets/images/desktop/poster_gallery/txt_066_2.png?"
             alt=""
           />
         </S.Text>
-        <S.Text top={40} right={8}>
+        <S.Text top={10} right={8}>
           <img
+            ref={TextRef2}
             src="https://gransaga.com/assets/images/desktop/poster_gallery/txt_066_1.png"
             alt=""
           />
@@ -35,7 +60,7 @@ export default function KeyArt() {
           />
         </S.Art>
         <S.KeyArtPictures>
-          <span>
+          <span ref={KeyArtPicturesRef}>
             <Slider {...settings}>
               {image.map((link) => (
                 <>
