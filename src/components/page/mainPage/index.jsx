64af65ba276beb6ab85d50679b315e5edc17c1e9
eastcopper <ldh7228@gmail.com>
2022-06-29 21:41:45 +0900

    ⚡ :: (#34)[MainPage] Update MainPage

--- src/components/page/mainPage/index.jsx
+++ src/components/page/mainPage/index.jsx
@@ -2,21 +2,39 @@ import React, { useRef } from "react";
 import * as S from "./styles";
 import { btn_dn } from "../../../lib/export/data";
 import BtnDn from "../../common/btn_dn";
+import { useSelector, useDispatch } from "react-redux";
+import { increment } from "../../../pagemove";
 
 export default function MainPage() {
   const arrowRef = useRef(null);
+  const titleRef = useRef(null);
+  const btnRef = useRef(null);
+  const page = useSelector((state) => state.movePage.page);
+  const dispatch = useDispatch();
 
-  const arrowActive = (bool) => {
-    if (bool) {
-      arrowRef.current.style =
-        "animation: updown 0.3s ease-in-out infinite alternate;";
+  if (
+    titleRef.current !== null &&
+    btnRef.current !== null &&
+    arrowRef.current !== null
+  ) {
+    if (page === 0) {
+      titleRef.current.classList.add("active");
+      btnRef.current.classList.add("active");
+      arrowRef.current.classList.add("active");
     } else {
-      arrowRef.current.style = "animation: none";
+      titleRef.current.classList.remove("active");
+      btnRef.current.classList.remove("active");
+      arrowRef.current.classList.remove("active");
     }
+  }
+
+  const moveDown = () => {
+    dispatch(increment(1));
+    window.location.href = "#pc";
   };
 
   return (
-    <S.Container>
+    <S.Container id="main">
       <S.BgVideo>
         <video muted loop autoPlay>
           <source
@@ -27,23 +45,35 @@ export default function MainPage() {
       </S.BgVideo>
       <S.Title>
         <img
+          className="active"
+          ref={titleRef}
           src="https://gransaga.com/assets/images/desktop/main/title_066.png"
           alt=""
         />
       </S.Title>
       <S.Btn_dn>
-        <BtnDn />
+        <div ref={btnRef} className="active">
+          <BtnDn />
+        </div>
       </S.Btn_dn>
-      <S.Scroll
-        onMouseOver={() => arrowActive(true)}
-        onMouseOut={() => arrowActive(false)}
-      >
-        <img
-          ref={arrowRef}
-          src="https://gransaga.com/assets/images/desktop/main/arrow_dn_2.png"
-          alt=""
-        />
-      </S.Scroll>
+      <div onClick={() => moveDown()}>
+        <S.Scroll
+          onMouseOver={(e) => {
+            e.currentTarget.children[0].classList.remove("active");
+            e.currentTarget.children[0].classList.add("active1");
+          }}
+          onMouseOut={(e) => {
+            e.currentTarget.children[0].classList.remove("active1");
+          }}
+        >
+          <img
+            ref={arrowRef}
+            className="active"
+            src="https://gransaga.com/assets/images/desktop/main/arrow_dn_2.png"
+            alt=""
+          />
+        </S.Scroll>
+      </div>
     </S.Container>
   );
 }
