68319cfa8159cbce7eb129d0995c97d86873ece3
eastcopper <ldh7228@gmail.com>
2022-07-02 23:57:37 +0900

    ⚡ :: (#35)[PCVersion] Update PCVersion

--- src/components/page/pcVersion/index.jsx
+++ src/components/page/pcVersion/index.jsx
@@ -1,31 +1,50 @@
-import React from "react";
+import React, { useRef } from "react";
 import BtnDn from "../../common/btn_dn";
 import * as S from "./styles";
+import { useSelector } from "react-redux";
 
 export default function PCVersion() {
+  const page = useSelector((state) => state.movePage.page);
+  const ContentRef = useRef(null);
+  const DownloadRef = useRef(null);
+
+  if (ContentRef.current !== null && DownloadRef.current !== null) {
+    if (page === 1) {
+      ContentRef.current.classList.add("active");
+      DownloadRef.current.classList.add("active");
+    } else {
+      ContentRef.current.classList.remove("active");
+      DownloadRef.current.classList.remove("active");
+    }
+  }
+
   return (
-    <S.Container>
+    <S.Container id="pc">
       <S.PCDiv>
         <S.Contents>
           <S.Content>
-            <span
-              style={{
-                fontSize: "40px",
-                fontFamily: "'Noto Sans KR', sans-serif",
-                letterSpacing: "-3px",
-              }}
-            >
-              그랑사가 언제 어디서나
-            </span>
-            <br />
-            <span>
-              <span style={{ color: "#496EE5" }}>다양한 플랫폼</span>
-              <span>에서 자유롭게!</span>
-            </span>
+            <div ref={ContentRef}>
+              <span
+                style={{
+                  fontSize: "40px",
+                  fontFamily: "'Noto Sans KR', sans-serif",
+                  letterSpacing: "-3px",
+                }}
+              >
+                그랑사가 언제 어디서나
+              </span>
+              <br />
+              <span>
+                <span style={{ color: "#496EE5" }}>다양한 플랫폼</span>
+                <span>에서 자유롭게!</span>
+              </span>
+            </div>
           </S.Content>
           <S.Download>
-            <p>게임 플레이 데이터가 모든 플랫폼에서 연동됩니다!</p>
-            <BtnDn />
+            <div ref={DownloadRef}>
+              <p>게임 플레이 데이터가 모든 플랫폼에서 연동됩니다!</p>
+              <BtnDn />
+            </div>
           </S.Download>
         </S.Contents>
         <S.Hr />
